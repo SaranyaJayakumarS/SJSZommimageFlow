4ce42798841757c54f8a9527aafa3c7c46722dae
Vimal Das <vimaldas@Vimals-Mac-mini.local>
2018-11-27 11:08:46 +0530

    Initial Commit

---
+++ DemoScrollZoom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoScrollZoom
+//
+//  Created by Saranya Jayakumar on 27/11/18.
+//  Copyright © 2018 Vimal Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
